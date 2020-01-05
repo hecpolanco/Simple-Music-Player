d4dc9abdb9e2b32481eace06c115d6e3035b05db
hdp2112 <hdp2112@columbia.edu>
2020-01-05 13:11:54 -0500

    music player html, css, and js file

---
+++ player.js
@@ -0,0 +1,105 @@
+const background = document.querySelector('#background'); // background derived from album cover below
+const thumbnail = document.querySelector('#thumbnail'); // album cover 
+const song = document.querySelector('#song'); // audio object
+
+const songArtist = document.querySelector('.song-artist'); // element where track artist appears
+const songTitle = document.querySelector('.song-title'); // element where track title appears
+const progressBar = document.querySelector('#progress-bar'); // element where progress bar appears
+let pPause = document.querySelector('#play-pause'); // element where play and pause image appears
+
+songIndex = 0;
+songs = ['./assets/music/beyonce.mp3', './assets/music/coldwar.mp3']; // object storing paths for audio objects
+thumbnails = ['./assets/images/lemonade.png', './assets/images/cautious.png']; // object storing paths for album covers and backgrounds
+songArtists = ['Beyonce', 'Cautious Clay']; // object storing track artists
+songTitles = ["Don't Hurt Yourself", "Cold War"]; // object storing track titles
+
+// function where pp (play-pause) element changes based on playing boolean value - if play button clicked, change pp.src to pause button and call song.play() and vice versa.
+let playing = true;
+function playPause() {
+    if (playing) {
+        const song = document.querySelector('#song'),
+        thumbnail = document.querySelector('#thumbnail');
+
+        pPause.src = "./assets/icons/pause.png"
+        thumbnail.style.transform = "scale(1.15)";
+        
+        song.play();
+        playing = false;
+    } else {
+        pPause.src = "./assets/icons/play.png"
+        thumbnail.style.transform = "scale(1)"
+        
+        song.pause();
+        playing = true;
+    }
+}
+
+// automatically play the next song at the end of the audio object's duration
+song.addEventListener('ended', function(){
+    nextSong();
+});
+
+// function where songIndex is incremented, song/thumbnail image/background image/song artist/song title changes to next index value, and playPause() runs to play next track 
+function nextSong() {
+    songIndex++;
+    if (songIndex > 1) {
+        songIndex = 0;
+    };
+    song.src = songs[songIndex];
+    thumbnail.src = thumbnails[songIndex];
+    background.src = thumbnails[songIndex];
+
+    songArtist.innerHTML = songArtists[songIndex];
+    songTitle.innerHTML = songTitles[songIndex];
+
+    playing = true;
+    playPause();
+}
+
+// function where songIndex is decremented, song/thumbnail image/background image/song artist/song title changes to previous index value, and playPause() runs to play previous track 
+function previousSong() {
+    songIndex--;
+    if (songIndex < 0) {
+        songIndex = 1;
+    };
+    song.src = songs[songIndex];
+    thumbnail.src = thumbnails[songIndex];
+    background.src = thumbnails[songIndex];
+
+    songArtist.innerHTML = songArtists[songIndex];
+    songTitle.innerHTML = songTitles[songIndex];
+
+    playing = true;
+    playPause();
+}
+
+// update progressBar.max to song object's duration, same for progressBar.value, update currentTime/duration DOM
+function updateProgressValue() {
+    progressBar.max = song.duration;
+    progressBar.value = song.currentTime;
+    document.querySelector('.currentTime').innerHTML = (formatSecondsAsTime(Math.floor(song.currentTime)));
+    if (document.querySelector('.durationTime').innerHTML === "NaN:NaN") {
+        document.querySelector('.durationTime').innerHTML = "0:00";
+    } else {
+        document.querySelector('.durationTime').innerHTML = (formatSecondsAsTime(Math.floor(song.duration)));
+    }
+};
+
+// convert song.currentTime and song.duration into MM:SS format
+function formatSecondsAsTime(secs) {
+    let hr  = Math.floor(secs / 3600);
+    let min = Math.floor((secs - (hr * 3600))/60);
+    let sec = Math.floor(secs - (hr * 3600) -  (min * 60));
+    if (sec < 10){ 
+        sec  = `0${sec}`;
+    };
+    return `${min}:${sec}`;
+};
+
+// run updateProgressValue function every 1/2 second to show change in progressBar and song.currentTime on the DOM
+setInterval(updateProgressValue, 500);
+
+// function where progressBar.value is changed when slider thumb is dragged without auto-playing audio
+function changeProgressBar() {
+    song.currentTime = progressBar.value;
+};
